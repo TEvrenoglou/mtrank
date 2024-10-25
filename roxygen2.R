2e266118dff6059b0dfb180bce0e7158f851126a
Guido Schwarzer <guido.schwarzer@uniklinik-freiburg.de>
2024-10-25 17:16:44 +0200

    Edit roxygen2.R

--- roxygen2.R
+++ roxygen2.R
@@ -1,6 +1,7 @@
 #
 # (1) Make R packages available
 #
+
 library("devtools")
 library("roxygen2")
 
@@ -8,12 +9,14 @@ library("roxygen2")
 #
 # (2) Create documentation file(s)
 #
+
 document()
 
 
 #
 # (3) Build R package and PDF file with help pages
 #
+
 build(args = "--compact-vignettes=gs+qpdf")
 build_manual()
 
@@ -21,10 +24,12 @@ build_manual()
 #
 # (4) Install R package
 #
+
 install(build_vignettes = TRUE)
 
 
 #
 # (5) Check R package
 #
+
 check(args = "--as-cran")
